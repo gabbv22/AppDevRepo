2f74945f27dc8c74f4e9df028e98234c32f9ba4a
gabbv? <gvalderama22@gmail.com>
2025-12-19 22:57:07 +0800

    Revert "Modified project: UI line fixes and added sentiment feature"
    This reverts commit 7d716d3f3072bcaed4583ee22019e97702bbd66a.

--- Activity#1/ConsoleApp2/Program.cs
+++ Activity#1/ConsoleApp2/Program.cs
@@ -8,36 +8,30 @@ namespace MyProject
         {
 
             Console.WriteLine("=====Tip Calculator=====");
+
             Console.WriteLine("\nPress ENTER to proceed.\n");
-            Console.WriteLine("==========================\n");
             Console.ReadKey();
 
-            Console.WriteLine("==========================\n");
+
             Console.WriteLine("Enter Customer's Bill: ");
             string billInput = Console.ReadLine();
             double billTotal = Convert.ToDouble(billInput);
 
-            Console.WriteLine("\n==========================\n");
-            Console.WriteLine("Thank you for dining in our restaurant!\n");
+
+            Console.WriteLine("\nThank you for dining in our restaurant!\n");
             Console.WriteLine("Your bill is: PHP" + billTotal);
 
-            Console.WriteLine("\n==========================\n");
-            Console.Write("How much tip would you like to give (%): ");
+            Console.Write("\nHow much tip would you like to give (%): ");
             string tipInput = Console.ReadLine();
             double tipPercent = Convert.ToDouble(tipInput);
 
-            Console.WriteLine("\n==========================\n");
             double tipAmount = billTotal * tipPercent / 100;
             double totalAmount = billTotal + tipAmount;
 
-            Console.WriteLine("\n==========================\n");
-            Console.WriteLine("Tip Amount: PHP" + tipAmount);
+            Console.WriteLine("\nTip Amount: PHP" + tipAmount);
             Console.WriteLine("Total Amount: PHP"+ totalAmount);
 
-            Console.WriteLine("\n==========================\n");
-            Console.WriteLine("Thank you for using Tip Calculator.");
-            Console.WriteLine("\nPress any key to exit.");
-            Console.WriteLine("\n==========================\n");
+            Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
 
         }
