28b9eaf9653adc4e140382e33561a1966ceac5e1
gabbv? <gvalderama22@gmail.com>
2025-12-19 22:16:48 +0800

    Initial commit: basic tip calculator

---
+++ Activity#1/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("=====Tip Calculator=====");
+
+            Console.WriteLine("\nPress ENTER to proceed.\n");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Enter Customer's Bill: ");
+            string billInput = Console.ReadLine();
+            double billTotal = Convert.ToDouble(billInput);
+
+
+            Console.WriteLine("\nThank you for dining in our restaurant!\n");
+            Console.WriteLine("Your bill is: PHP" + billTotal);
+
+            Console.Write("\nHow much tip would you like to give (%): ");
+            string tipInput = Console.ReadLine();
+            double tipPercent = Convert.ToDouble(tipInput);
+
+            double tipAmount = billTotal * tipPercent / 100;
+            double totalAmount = billTotal + tipAmount;
+
+            Console.WriteLine("\nTip Amount: PHP" + tipAmount);
+            Console.WriteLine("Total Amount: PHP"+ totalAmount);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
